5a1aee726909a5b803a16e51b529f5347f467d77
aaronontheweb <aaron@aaronstannard.com>
2011-08-05 19:51:00 -0700

    Updated assembly information.

--- MVC.Utilities/Properties/AssemblyInfo.cs
+++ MVC.Utilities/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MVC.Utilities")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Open source utilities for ASP.NET MVC developers")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Aaron Stannard")]
 [assembly: AssemblyProduct("MVC.Utilities")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCopyright("Copyright © Aaron Stannard 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
