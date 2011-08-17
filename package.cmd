b73d5dcfe4852b16859edb1fbfbff5937b724fb3
Aaron Stannard <aaron@aaronstannard.com>
2011-08-16 21:16:48 -0700

    Fixed a number of bugs with the NuGet packages

--- package.cmd
+++ package.cmd
@@ -5,13 +5,13 @@ if not exist Download\packages\MVC.Utilities\lib mkdir Download\packages\MVC.Uti
 if not exist Download\packages\MVC.Utilities\lib\net4 mkdir Download\packages\MVC.Utilities\lib\net4
 if not exist Download\packages\MVC.Utilities.Azure mkdir Download\packages\MVC.Utilities.Azure
 if not exist Download\packages\MVC.Utilities.Azure\lib mkdir Download\packages\MVC.Utilities.Azure\lib
-if not exist Download\packages\MVC.Utilities.Azure\net4 mkdir Download\packages\MVC.Utilities.Azure\net4
+if not exist Download\packages\MVC.Utilities.Azure\lib\net4 mkdir Download\packages\MVC.Utilities.Azure\lib\net4
 
 copy LICENSE.txt Download\packages\MVC.Utilities
 copy LICENSE.txt Download\packages\MVC.Utilities.Azure
 
 copy MVC.Utilities\bin\Release\MVC.Utilities.dll Download\packages\MVC.Utilities\lib\net4
-copy MVC.Utilities.Azure\bin\Release\MVC.Utilities.Azure.dll Download\packages\MVC.Utilities.Azure\net4
+copy MVC.Utilities.Azure\bin\Release\MVC.Utilities.Azure.dll Download\packages\MVC.Utilities.Azure\lib\net4
 
 nuget.exe pack mvc-utilities.nuspec -b Download\packages\MVC.Utilities -o Download
 nuget.exe pack mvc-utilities-azure.nuspec -b Download\packages\MVC.Utilities.Azure -o Download
