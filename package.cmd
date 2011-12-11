7d3245e1317525aab696b6db42404ace6803e2dc
Aaron Stannard <aaron@aaronstannard.com>
2011-12-10 17:54:37 -0800

    Updated the batch file used for maintaining all of the NuGet packages....

--- package.cmd
+++ package.cmd
@@ -6,12 +6,18 @@ if not exist Download\packages\MVC.Utilities\lib\net4 mkdir Download\packages\MV
 if not exist Download\packages\MVC.Utilities.Azure mkdir Download\packages\MVC.Utilities.Azure
 if not exist Download\packages\MVC.Utilities.Azure\lib mkdir Download\packages\MVC.Utilities.Azure\lib
 if not exist Download\packages\MVC.Utilities.Azure\lib\net4 mkdir Download\packages\MVC.Utilities.Azure\lib\net4
+if not exist Download\packages\MVC.Utilities.BCrypt mkdir Download\packages\MVC.Utilities.BCrypt
+if not exist Download\packages\MVC.Utilities.BCrypt\lib mkdir Download\packages\MVC.Utilities.BCrypt\lib
+if not exist Download\packages\MVC.Utilities.BCrypt\lib\net4 mkdir Download\packages\MVC.Utilities.BCrypt\lib\net4
 
 copy LICENSE.txt Download\packages\MVC.Utilities
 copy LICENSE.txt Download\packages\MVC.Utilities.Azure
+copy LICENSE.txt Download\packages\MVC.Utilities.BCrypt
 
 copy MVC.Utilities\bin\Release\MVC.Utilities.dll Download\packages\MVC.Utilities\lib\net4
 copy MVC.Utilities.Azure\bin\Release\MVC.Utilities.Azure.dll Download\packages\MVC.Utilities.Azure\lib\net4
+copy MVC.Utilities.BCrypt\bin\Release\MVC.Utilities.BCrypt.dll Download\packages\MVC.Utilities.BCrypt\lib\net4
 
 nuget.exe pack mvc-utilities.nuspec -b Download\packages\MVC.Utilities -o Download
 nuget.exe pack mvc-utilities-azure.nuspec -b Download\packages\MVC.Utilities.Azure -o Download
+nuget.exe pack mvc-utilities-bcrypt.nuspec -b Download\packages\MVC.Utilities.BCrypt -o Download
