6c83fa4074439c0b290439b58b01be5e1fd3d89b
aaronontheweb <aaron@aaronstannard.com>
2012-02-15 14:29:10 -0800

    Extended the ICryptoService interface to include elements for salted hashes

--- MVC.Utilities/Encryption/ICryptoService.cs
+++ MVC.Utilities/Encryption/ICryptoService.cs
@@ -8,9 +8,17 @@
         /// <summary>
         /// Hashes the original string used whatever mechanism is implemented by the interface
         /// </summary>
-        /// <param name="originalStr">the original string</param>
+        /// <param name="originalStr">a plaintext string</param>
         /// <returns>a hashed string</returns>
         string HashPassword(string originalStr);
+
+        /// <summary>
+        /// Hashes the original string plus its salt used whatever mechanism is implemented by the interface
+        /// </summary>
+        /// <param name="originalStr">a plaintext string</param>
+        /// <param name="salt">a string containing a salt used to create the hash</param>
+        /// <returns>a hashed string</returns>
+        string HashPassword(string originalStr, string salt);
 
         /// <summary>
         /// Checks the two strings to see if the hashes are equivalent
@@ -19,5 +27,15 @@
         /// <param name="hashed">The second value to check</param>
         /// <returns>Returns true if the hashes are equivalent in accordance with the crypto implementation</returns>
         bool CheckPassword(string plainText, string hashed);
+
+
+        /// <summary>
+        /// Checks the two strings to see if the hashes are equivalent
+        /// </summary>
+        /// <param name="plainText">The first value to check</param>
+        /// <param name="hashed">The second value to check</param>
+        /// <param name="salt">a string containing the salt used to create the hash</param>
+        /// <returns>Returns true if the hashes are equivalent in accordance with the crypto implementation</returns>
+        bool CheckPassword(string plainText, string hashed, string salt);
     }
 }
