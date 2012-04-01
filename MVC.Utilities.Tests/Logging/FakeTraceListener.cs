9859930d1edec808ba21031252ac27bd3b2c9699
Aaron Stannard <aaron@aaronstannard.com>
2012-04-01 15:28:05 -0700

    Implementing unit tests for validating tracing

---
+++ MVC.Utilities.Tests/Logging/FakeTraceListener.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using MVC.Utilities.Logging;
+
+namespace MVC.Utilities.Tests.Logging
+{
+    public class FakeTraceListener : TraceListener
+    {
+        public IList<string> Messages { get; private set; }
+        public string LastObservedCategory { get; set; }
+
+        public FakeTraceListener()
+        {
+            Messages = new List<string>();
+        }
+
+        #region Overrides of TraceListener
+
+        public override void WriteLine(object o, string category)
+        {
+            Messages.Add(o.ToString());
+            LastObservedCategory = category;
+        }
+
+        public override void Write(string message)
+        {
+            Messages.Add(message);
+        }
+
+        public override void WriteLine(string message)
+        {
+            Messages.Add(message);
+        }
+
+        #endregion
+    }
+}
--- MVC.Utilities.Tests/Logging/TraceLoggingServiceTests.cs
+++ MVC.Utilities.Tests/Logging/TraceLoggingServiceTests.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using MVC.Utilities.Logging;
 using NUnit.Framework;
 
 namespace MVC.Utilities.Tests.Logging
@@ -9,5 +11,54 @@ namespace MVC.Utilities.Tests.Logging
     [TestFixture(Description = "Tests aimed to ensure that our trace logging service works as expected")]
     public class TraceLoggingServiceTests
     {
+        private TraceLoggingService _traceLogging;
+        private FakeTraceListener _fakeTraceListener;
+
+        public TraceLoggingServiceTests()
+        {
+            _traceLogging = new TraceLoggingService();
+            _fakeTraceListener = new FakeTraceListener();
+        }
+
+        #region Setup / Teardown
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            //Clear all of the current trace listeners
+            Trace.Listeners.Clear();
+
+            //Listen to just our fake trace listener
+            Trace.Listeners.Add(_fakeTraceListener);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            //Clear all of the messages collected in our Fake trace listener
+            _fakeTraceListener.Messages.Clear();
+            _fakeTraceListener.LastObservedCategory = String.Empty;
+        }
+
+        #endregion
+
+        #region Tests
+
+        [Test(Description = "Should log a simple message to our trace listener")]
+        public void ShouldLogToTrace()
+        {
+            var message = "Hi there!";
+            _traceLogging.LogMessage(message);
+
+            Assert.IsTrue(_fakeTraceListener.Messages.All(x => x.Equals(message)));
+
+            _traceLogging.LogMessage(message, LogEntryType.Success);
+
+            //Setting a category flag should not impact the contents of the message itself
+            Assert.IsTrue(_fakeTraceListener.Messages.All(x => x.Equals(message)));
+            Assert.AreEqual(_fakeTraceListener.LastObservedCategory, "Success", string.Format("Expected 'Success', not {0}", _fakeTraceListener.LastObservedCategory));
+        }
+
+        #endregion
     }
 }
