e5370aa99dd4bdea2074f3fe53701bf3aedb589d
Aaron Stannard <aaron@aaronstannard.com>
2012-04-01 15:11:34 -0700

    Upgraded NUnit

---
+++ MVC.Utilities.Tests/Logging/TraceLoggingServiceTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace MVC.Utilities.Tests.Logging
+{
+    [TestFixture(Description = "Tests aimed to ensure that our trace logging service works as expected")]
+    public class TraceLoggingServiceTests
+    {
+    }
+}
--- MVC.Utilities/Logging/EventLogLoggingService.cs
+++ MVC.Utilities/Logging/EventLogLoggingService.cs
@@ -1,10 +1,11 @@
+using System;
 using System.Diagnostics;
 
 namespace MVC.Utilities.Logging
 {
     public class EventLogLoggingService : ILoggingService
     {
-        private EventLog log;
+        private readonly EventLog _log;
 
         public EventLogLoggingService(string eventLogSource) : this(eventLogSource, "Application"){}
 
@@ -15,17 +16,51 @@ namespace MVC.Utilities.Logging
                 EventLog.CreateEventSource(eventLogSource, logName);
             }
 
-            log = new EventLog(logName, ".", eventLogSource);
+            _log = new EventLog(logName, ".", eventLogSource);
         }
 
         #region Implementation of ILoggingService
 
         public void LogMessage(string message)
         {
-            log.WriteEntry(message);
+            _log.WriteEntry(message);
+        }
+
+        public void LogMessage(string message, LogEntryType entryType)
+        {
+            _log.WriteEntry(message, MapLogEntryType(entryType));
+        }
+
+        public void LogException(Exception ex)
+        {
+            _log.WriteEntry(ex.ToTraceString(), EventLogEntryType.Error);
+        }
+
+        public void LogException(Exception ex, LogEntryType entryType)
+        {
+            _log.WriteEntry(ex.ToTraceString(), MapLogEntryType(entryType));
         }
 
         #endregion
+
+        public static EventLogEntryType MapLogEntryType(LogEntryType entryType)
+        {
+            switch(entryType)
+            {
+                case LogEntryType.Error:
+                    return EventLogEntryType.Error;
+                case LogEntryType.Failure:
+                    return EventLogEntryType.FailureAudit;
+                case LogEntryType.Success:
+                    return EventLogEntryType.SuccessAudit;
+                case LogEntryType.Information:
+                    return EventLogEntryType.Information;
+                case LogEntryType.Warning:
+                    return EventLogEntryType.Warning;
+                default:
+                    return EventLogEntryType.Information;
+            }
+        }
 
     }
 }
