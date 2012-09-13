a442c1707ebfd94001de8e7fe628881a6653d2b2
Aaronontheweb <aaron@aaronstannard.com>
2012-09-13 12:11:07 -0700

    working on fleshing out the projects hash

---
+++ buildscripts/projects.rb
@@ -0,0 +1,9 @@
+#----------------------------------
+# Project data for MVC.Utilities
+#----------------------------------
+
+Projects => {
+	:language = "en-US",
+	:project_url = "https://github.com/Aaronontheweb/mvc-utilities",
+	:license_url = "https://github.com/Aaronontheweb/mvc-utilities/blob/master/license.txt"
+}
